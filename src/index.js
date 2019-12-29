b2e6293c26c9aeb65da5d3bb59c5501dcc8d6b70
David Jacobsen <dgrjacobsen@gmail.com>
2019-12-29 04:10:00 +0100

    Initial App Structure - Home, About, Courses

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import {render} from 'react-dom';
+import {BrowserRouter as Router} from 'react-router-dom';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css"
+import App from "./components/App";
 
-const Hi = () => <p>Hi.</p>;
-
-render(<Hi/>, document.getElementById("app"));
+render(
+    <Router>
+        <App/>
+    </Router>, document.getElementById("app"));
